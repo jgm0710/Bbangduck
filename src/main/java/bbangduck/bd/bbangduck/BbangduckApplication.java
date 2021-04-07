7c317293a9d918a0954fa754b8580c298dd2ef09
Gumin Jeong <jgm0710@gmail.com>
2021-04-07 12:58:31 +0900

    init project

---
+++ src/main/java/bbangduck/bd/bbangduck/BbangduckApplication.java
@@ -0,0 +1,13 @@
+package bbangduck.bd.bbangduck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbangduckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BbangduckApplication.class, args);
+    }
+
+}
---
+++ src/test/java/bbangduck/bd/bbangduck/BbangduckApplicationTests.java
@@ -0,0 +1,13 @@
+package bbangduck.bd.bbangduck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbangduckApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
