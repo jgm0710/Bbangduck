2bac303d3207738532203dc1190f814d8f52dbd4
Gumin Jeong <jgm0710@gmail.com>
2021-05-02 16:27:37 +0900

    MemberApiController 변경

--- src/main/java/bbangduck/bd/bbangduck/domain/member/controller/MemberApiController.java
+++ src/main/java/bbangduck/bd/bbangduck/domain/member/controller/MemberApiController.java
@@ -1,4 +1,11 @@
 package bbangduck.bd.bbangduck.domain.member.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/members")
 public class MemberApiController {
 }
