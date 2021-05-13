bf54f2fa1490c0b6ebb7cd4d589c14b83aca0c15
Gumin Jeong <jgm0710@gmail.com>
2021-05-13 18:52:07 +0900

    AuthApiController 약간 수정

--- src/main/java/bbangduck/bd/bbangduck/domain/auth/controller/AuthApiController.java
+++ src/main/java/bbangduck/bd/bbangduck/domain/auth/controller/AuthApiController.java
@@ -17,10 +17,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.net.URI;
@@ -72,6 +69,7 @@ public class AuthApiController {
         hasErrorsThrow(errors, ResponseStatus.REFRESH_NOT_VALID);
         TokenDto tokenDto = authenticationService.refresh(onlyRefreshTokenDto.getRefreshToken());
 
+        log.info("Refresh sign in success");
         return ResponseEntity.ok(new ResponseDto<>(ResponseStatus.REFRESH_SIGN_IN_SUCCESS, tokenDto));
     }
 
