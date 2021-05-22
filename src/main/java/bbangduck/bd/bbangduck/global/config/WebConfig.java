1087ee4cbd00b27b2a2e1afbf9bdcae0040fd9bd
Gumin Jeong <jgm0710@gmail.com>
2021-05-22 17:11:58 +0900

    Cross Origin 설정 변경

--- src/main/java/bbangduck/bd/bbangduck/global/config/WebConfig.java
+++ src/main/java/bbangduck/bd/bbangduck/global/config/WebConfig.java
@@ -15,6 +15,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080");
+                .allowedOrigins("http://localhost:8080", "http://localhost:8081", "http://localhost:3000")
+                .allowCredentials(true);
     }
 }
