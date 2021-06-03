5e6fb9ec48cdb83b3d56f51be92f330ed3c32280
otorodevym <otrodevym@gamil.com>
2021-06-03 11:38:04 +0900

    Init : Develop branch 생성
    - mr에서 삭제 된 브런치 생성

--- src/main/java/bbangduck/bd/bbangduck/domain/search/repository/MemberSearchRepositoryImpl.java
+++ src/main/java/bbangduck/bd/bbangduck/domain/search/repository/MemberSearchRepositoryImpl.java
@@ -1,6 +1,16 @@
 package bbangduck.bd.bbangduck.domain.search.repository;
 
+import bbangduck.bd.bbangduck.domain.search.entity.MemberSearch;
+import bbangduck.bd.bbangduck.domain.search.entity.QMemberSearch;
+import com.querydsl.core.types.dsl.Expressions;
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.List;
 
 /**
  * Project : bbangduck
@@ -11,5 +21,22 @@ import org.springframework.stereotype.Repository;
  */
 
 @Repository
+@RequiredArgsConstructor
 public class MemberSearchRepositoryImpl implements MemberSearchRepositoryCustom{
+
+    private final JPAQueryFactory queryFactory;
+    
+    
+    @Override
+    public List<MemberSearch> searchTopMonthList() {
+        QMemberSearch qMemberSearch = QMemberSearch.memberSearch;
+
+        return null;
+//        return queryFactory.select(qMemberSearch.searchKeyword, qMemberSearch.searchType).from(qMemberSearch)
+//                .where(qMemberSearch.searchDate.after(Expressions.dateTemplate(LocalDate.class, "{0}",
+//                        LocalDate.now().minusMonths(1))))
+//                .fetch();
+    }
+
+
 }
