2758a5838138eda89ffbe0f6132b0355824528d4
Gumin Jeong <jgm0710@gmail.com>
2021-06-16 00:20:58 +0900

    Fix - EventTestContoller active profile test 지정

--- src/test/java/bbangduck/bd/bbangduck/domain/event/EventTestContoller.java
+++ src/test/java/bbangduck/bd/bbangduck/domain/event/EventTestContoller.java
@@ -27,6 +27,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -41,6 +42,7 @@ import java.util.Set;
 @ExtendWith(SpringExtension.class)
 @Transactional
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 public class EventTestContoller {
 
     @Autowired
